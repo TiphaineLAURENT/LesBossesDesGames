1e6e54d940ed58473b2ae14f182a90aaae921e80
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-09-26 18:31:32 +0200

    Add shoot down arrow to Player

--- Dissocié/Assets/Scripts/PlayerBehaviour.cs
+++ Dissocié/Assets/Scripts/PlayerBehaviour.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(Sprite))]
 public class PlayerBehaviour : MonoBehaviour
 {
-        void Update()
+        [SerializeField]
+        [RequireNotNull]
+        GameObject projectile;
+
+        private void Update()
         {
                 var positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
                 var mouseOnScreen = Camera.main.ScreenToViewportPoint(Input.mousePosition);
@@ -10,10 +16,20 @@ public class PlayerBehaviour : MonoBehaviour
                 var angle = AngleBetweenPoints(positionOnScreen, mouseOnScreen) + 90;
 
                 transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+                if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+                {
+                        Shoot();
+                }
         }
 
-        float AngleBetweenPoints(Vector3 a, Vector3 b)
+        private float AngleBetweenPoints(Vector3 a, Vector3 b)
         {
-                 return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+                return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        }
+
+        private void Shoot()
+        {
+                Instantiate(projectile, transform.position, transform.rotation);
         }
 }
