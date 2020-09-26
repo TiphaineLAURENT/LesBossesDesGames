a46c68140e1905798385c1eb12d670a1ce787e46
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-09-26 17:11:09 +0200

    Add player rotation on cursor movement

---
+++ Dissocié/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+        void Update()
+        {
+                var positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+                var mouseOnScreen = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+                var angle = AngleBetweenPoints(positionOnScreen, mouseOnScreen) + 90;
+
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+
+        float AngleBetweenPoints(Vector3 a, Vector3 b)
+        {
+                 return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        }
+}
