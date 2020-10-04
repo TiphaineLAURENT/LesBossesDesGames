8df4e1df6c0a558c6f28dca4a67a0f30e9613ec3
Nifu-hub <71068389+Nifu-hub@users.noreply.github.com>
2020-10-04 18:19:57 +0200

    Base Game-made by Victor

---
+++ Victor/Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.3f);
+        Destroy(gameObject);
+    }
+}
